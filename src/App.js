320053a6fbe9808b295603e3cfc8124ccae7fef8
Prahalad Belavadi <github@belavadi.com>
2020-11-28 16:54:27 +0530

    fetch data from api

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ function App() {
   const APP_ID = "1d76bf64";
   const APP_KEY = "95b14140b29e04325c2b2ab7b032493f";
 
+  const [recipes, setRecipes] = useState( useState( [] ) );
 
   // Sample API Request call: curl "https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
-
-
-  // const [state, setState] = useState( 0 )
-
-  const [counter, setCounter] = useState( 0 );
+  const endpoint = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
 
   useEffect( () => {
-    console.log( 'Effect has run' )
-  }, [counter] )
+    getRecipes()
+  }, [] )
 
+  const getRecipes = async () => {
+    const response = await fetch( endpoint );
+    const data = await response.json();
+    setRecipes( data.hits )
+  }
+
+  console.log( recipes )
   return (
     <div className="App">
 
@@ -26,7 +29,6 @@ function App() {
         <input type="text" className="search-bar" />
         <button type="submit" className="search-button">Search</button>
       </form>
-      <h1 onClick={() => { setCounter( counter + 1 ) }}>{counter}</h1>
 
     </div>
   );
